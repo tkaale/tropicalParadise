abfafc7065d01536eb6ebc87e3888307f3ad9d44
Aleksandra <tkaczyk.aleksandra@outlook.com>
2021-03-07 22:03:54 +0100

    js bars function(changing color for black on white setion)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const navBtn = document.querySelector('.burger-btn');
 const nav = document.querySelector('.nav');
 const navItem = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const menuStart = () => {
     nav.classList.toggle('nav--active');
+    navBtnBars.classList.remove('black-bars-color');
 
     navItem.forEach(item => {
         item.addEventListener('click', () => {
@@ -24,4 +28,22 @@ const handleNavItemNavigation = () => {
     });
 };
 
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+    allSections.forEach(section => {
+        if (section.classList.contains("white-section") && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains("white-section") && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
+    })
+}
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+handleCurrentYear();
 navBtn.addEventListener('click', menuStart);
+window.addEventListener('scroll', handleObserver)
